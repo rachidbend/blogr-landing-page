4647fc8bf3e5f64091e0748ab97e00e47a10e8dd
rachidbend <rachidbendaghor@gmail.com>
2022-08-18 17:01:11 +0100

    now dropdowns close if the body in clicked, and only one opens when clicked and the rest is closed

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 // making navigation open and close
 const btnNavEl = document.querySelector(".btn-nav");
 const bodyEl = document.querySelector("body");
+const dropdowns = document.querySelectorAll(".nav__list--tab");
+const arrows = document.querySelectorAll(".lab__link--arrow");
 
 btnNavEl.addEventListener("click", (e) => {
   e.preventDefault();
@@ -17,6 +19,7 @@ navTabLinkEls.forEach((link) => {
   link.addEventListener("click", (e) => {
     //  prevent link from refreshing page
     e.preventDefault();
+    resetDropdown();
     const arrowEl = e.target.lastChild;
     const tab = e.target.nextElementSibling;
     tab.classList.toggle("tab-open");
@@ -24,7 +27,7 @@ navTabLinkEls.forEach((link) => {
   });
 });
 
-// closing nav when body is clicked
+// closing nav when body is clicked and closing dropdowns
 bodyEl.addEventListener("click", (e) => {
   // if the target is not the nav button or nav itself, then close the nav
   if (!e.target.closest(".nav--main") && !e.target.closest(".btn-nav")) {
@@ -32,4 +35,20 @@ bodyEl.addEventListener("click", (e) => {
       bodyEl.classList.remove("nav-open");
     }
   }
+
+  //  if the body is clicked but not the main nav or the dropdown itself then close all dropdowns
+  if (!e.target.closest(".nav--main")) {
+    resetDropdown();
+  }
 });
+
+console.log(dropdowns);
+
+// if the nav is clicked keep the one clikced open but close the rest
+
+const resetDropdown = function () {
+  dropdowns.forEach((dropdown) => {
+    dropdown.classList.remove("tab-open");
+  });
+  arrows.forEach((arrow) => arrow.classList.remove("arrow-open"));
+};
